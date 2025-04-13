b0da69c762f529c66a9e98495d5f84cb02ea984c
SilDan <silvandaniel@web.de>
2025-04-13 18:32:20 +0200

    calc-1: Calculation of the sequence z^2+c. Closes #1
    Co-Authored-By: yihanshen-elsa <207056542+yihanshen-elsa@users.noreply.github.com>

--- src/mandelbrot/Main.java
+++ src/mandelbrot/Main.java
@@ -2,19 +2,33 @@ package mandelbrot;
 
 public class Main {
     public static void main(String[] args) {
-        ComplexNumbers z1 = new ComplexNumbers(3, 2);
-        ComplexNumbers z2 = new ComplexNumbers(1, -4);
+        ComplexNumbers c1 = new ComplexNumbers(0.3, 0.2);
+        ComplexNumbers c2 = new ComplexNumbers(1, -4);
 
-        ComplexNumbers summe12 = z1.add(z2); //z1 + z2
-        ComplexNumbers summe21 = z2.add(z1); //z2 + z1
+        ComplexNumbers summe12 = c1.add(c2); //c1 + c2
+        ComplexNumbers summe21 = c2.add(c1); //c2 + c1
         
-        ComplexNumbers produkt = z1.multiply(z2);
+        ComplexNumbers produkt = c1.multiply(c2);
+        ComplexNumbers z = new ComplexNumbers(0, 0);
+        
+        for(int n = 0 ; n<100 ; n++)
+        {
+        	z=recursive_step(z,c1);
+            System.err.println("z_: " +n+z);
 
-        System.out.println("z1: " + z1);
-        System.out.println("z2: " + z2);
+        }
+        
+        
+        System.out.println("c1: " + c1);
+        System.out.println("c2: " + c2);
         System.out.println("Summe: " + summe21);
         System.out.println("Summe: " + summe12);
         System.out.println("Produkt: " + produkt);
+    }
+    
+    static ComplexNumbers  recursive_step(ComplexNumbers zn, ComplexNumbers c)
+    {
+    	return zn.multiply(zn).add(c); 
     }
 }
 

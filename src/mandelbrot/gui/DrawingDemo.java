9ba44788c9bef280202413ca6cbfb1b658333d0b
SilDan <silvandaniel@web.de>
2025-04-14 12:00:40 +0200

    gui-3: Adding Coordinates to the pane. Relates to #4

--- src/mandelbrot/gui/DrawingDemo.java
+++ src/mandelbrot/gui/DrawingDemo.java
@@ -5,6 +5,7 @@ import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 import mandelbrot.calc.ComplexNumbers;
 
@@ -36,12 +37,33 @@ public class DrawingDemo extends Application {
         double scaleX = (width - 2 * padding) / (maxX - minX);
         double scaleY = (height - 2 * padding) / (maxY - minY);
 
-        for (ComplexNumbers z : points) {
+        // Zeichne das Koordinatensystem (Achsen)
+        Line xAxis = new Line(padding, height / 2, width - padding, height / 2);
+        Line yAxis = new Line(width / 2, padding, width / 2, height - padding);
+        xAxis.setStroke(Color.BLACK);
+        yAxis.setStroke(Color.BLACK);
+        root.getChildren().addAll(xAxis, yAxis);
+
+        // Zeichne Punkte und Linien
+        for (int i = 0; i < points.size(); i++) {
+            ComplexNumbers z = points.get(i);
             double x = padding + (z.getReal() - minX) * scaleX;
             double y = padding + (maxY - z.getImag()) * scaleY; // y-Achse umdrehen
 
-            Circle point = new Circle(x, y, 5, Color.RED);
+            Circle point = new Circle(x, y, 2, Color.RED); // kleinere Punkte
             root.getChildren().add(point);
+
+            // Linie zum nächsten Punkt (falls vorhanden)
+            if (i < points.size() - 1) {
+                ComplexNumbers nextZ = points.get(i + 1);
+                double nextX = padding + (nextZ.getReal() - minX) * scaleX;
+                double nextY = padding + (maxY - nextZ.getImag()) * scaleY;
+                
+                Line line = new Line(x, y, nextX, nextY); // Linie zwischen den Punkten
+                line.setStroke(Color.BLACK);
+                line.setStrokeWidth(1); // dünne Linie
+                root.getChildren().add(line);
+            }
         }
 
         Scene scene = new Scene(root, width, height);
